fdf54b588d0ce1db0d1b66eb375a4d20808fc3eb
Eugen <negativ529021@gmail.com>
2018-11-15 17:59:52 +0200

    Остался трабл, - ридер не читает строку при тестировании

--- src/test/java/ua/com/juja/integrationTest/integrationTest.java
+++ src/test/java/ua/com/juja/integrationTest/integrationTest.java
@@ -1,19 +1,45 @@
 package ua.com.juja.integrationTest;
 
 import org.junit.BeforeClass;
+import org.junit.Test;
+import ua.com.juja.controller.Main;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
 
 
 public class integrationTest {
     private static ConfigurableInputStream in;
-    private static LogPrintStream out;
+    private static LogOutputStream out;
+    private static InputStream inStrean;
 
     @BeforeClass
     public static void setup() {
+        in = new ConfigurableInputStream();
+        out = new LogOutputStream();
+    }
+    @Test
+    public void testExit(){
+        //given
+        inStrean = new ByteArrayInputStream("exit".getBytes());
+        System.setIn(inStrean);
 
-        System.setIn(in);
-        System.setOut(new PrintStream(out));
+        //when
+        Main.main(new String[0]);
+
+        //then
+        assertEquals("", out.getLog());
+
+    }
+
+    @Test
+    public void testHelp(){
+        in.setLine("help");
+        Main.main(new String[0]);
+        assertEquals("",out.getLog());
     }
 }
