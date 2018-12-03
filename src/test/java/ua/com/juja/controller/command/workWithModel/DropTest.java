371d92f3c9f5842eec552337680c194014bc4d87
Eugene Vlasov <negativ529021@gmail.com>
2018-12-04 00:35:25 +0200

    Сделано: юнит-тесты на drop

--- src/test/java/ua/com/juja/controller/command/workWithModel/DropTest.java
+++ src/test/java/ua/com/juja/controller/command/workWithModel/DropTest.java
@@ -1,15 +1,17 @@
 package ua.com.juja.controller.command.workWithModel;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
-import org.mockito.Mockito;
+
+import static org.mockito.Mockito.*;
 import ua.com.juja.controller.command.Command;
+import ua.com.juja.model.exceptions.UnknowTableException;
 import ua.com.juja.model.parentClassesAndInterfaces.ModelInterface;
 import ua.com.juja.view.ViewInterface;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 import static org.junit.Assert.*;
 
@@ -21,8 +23,8 @@ public class DropTest {
 
     @Before
     public void setup() {
-        model = Mockito.mock(ModelInterface.class);
-        view = Mockito.mock(ViewInterface.class);
+        model = mock(ModelInterface.class);
+        view = mock(ViewInterface.class);
         drop = new Drop(model, view);
     }
 
@@ -44,7 +46,7 @@ public class DropTest {
         String[] params = new String[]{"drop", "users"};
         drop.doWork(params, connectionToDB);
         ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
-        Mockito.verify(view).setMessage(captor.capture());
+        verify(view).setMessage(captor.capture());
         assertEquals(expected, captor.getValue());
     }
 
@@ -55,9 +57,60 @@ public class DropTest {
         String[] params = new String[]{"drop"};
         drop.doWork(params, connectionToDB);
         ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
-        Mockito.verify(view).setMessage(captor.capture());
+        verify(view).setMessage(captor.capture());
         assertEquals(expected, captor.getValue());
     }
 
+    @Test
+    public void testDoWorkWithUnknowTableException() throws SQLException {
+        String expected = "Вы попытались удалить несуществующую таблицу.\n" +
+                "Введите команду 'tables', чтобы увидеть все созданные таблицы";
+        String[] commandForWork = new String[]{"clear", "user"};
+        try {
+            doThrow(new UnknowTableException()).when(model).drop(commandForWork, connectionToDB);
+        } catch (UnknowTableException e) {
+            //do nothing
+        }
+        drop.doWork(commandForWork, connectionToDB);
+        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+        verify(view).setMessage(captor.capture());
+        assertEquals(expected, captor.getValue());
 
+    }
+
+    //When .doWork() receives NullPointerException
+    @Test
+    public void testDoWorkWithNullPointerException() throws SQLException, UnknowTableException {
+        String expected = "Вы попытались удалить таблицу, не подключившись к базе данных.\n" +
+                "Подключитесь к базе данных командой\n" +
+                "'connect|database|username|password'";
+        String[] commandForWork = new String[]{"drop", "user"};
+        try {
+            doThrow(new NullPointerException()).when(model).drop(commandForWork, connectionToDB);
+        } catch (NullPointerException e) {
+            //do nothing
+        }
+        drop.doWork(commandForWork, connectionToDB);
+        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+        verify(view).setMessage(captor.capture());
+        assertEquals(expected, captor.getValue());
+
+    }
+
+    @Test
+    public void testDoWorkWithSQLException() throws UnknowTableException {
+        String expected = "Неизвестная ошибка при попытке связаться с базой данных.\n" +
+                "Причина: null";
+        String[] commandForWork = new String[]{"clear", "user"};
+        try {
+            doThrow(new SQLException()).when(model).drop(commandForWork, connectionToDB);
+        } catch (SQLException e) {
+            //do nothing
+        }
+        drop.doWork(commandForWork, connectionToDB);
+        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+        verify(view).setMessage(captor.capture());
+        assertEquals(expected, captor.getValue());
+
+    }
 }
